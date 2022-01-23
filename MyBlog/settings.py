506f9745e3264ca8927fc816f9a276e1ee629b51
chenrb <crbxyz@foxmail.com>
2022-01-23 16:26:33 +0800

    feature: 去除xadmin

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -16,9 +16,6 @@ import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# 把apps目录加到搜索目录之下，否则命令行运行时将找不到
-sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -43,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'xadmin',
     'crispy_forms',
     'pure_pagination',
     'markdown',
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MyBlog
 个人博客  
-[http://www.chenrb.xyz/](http://www.chenrb.xyz/)
+[http://www.chenrb.xyz/](http://www.chenrb.xyz/)  (已废弃)
+
